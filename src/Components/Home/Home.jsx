77157bb7aca371d630c035afa1467e1b95c30b35
parshyagk23 <gkprashant52@gmail.com>
2024-03-25 19:09:06 +0530

    add handleclear

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -57,7 +57,7 @@ const Home = () => {
           >
             <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
           </svg>
-          <input className="font-medium text-2xl text-slate-400 outline-none w-full xs:text-xl"  type="text" name=""  placeholder="Type any job title" onChange={(e)=>{setTitle(e.target.value)}} />
+          <input className="font-medium text-2xl text-slate-400 outline-none w-full xs:text-xl"  type="text" name="" value={title}  placeholder="Type any job title" onChange={(e)=>{setTitle(e.target.value)}} />
         </div>
         <section className="flex gap-4 justify-between lg:flex-col xs:flex-col  md:flex-col  ">
           <div className="flex h-fit gap-5 mt-4 xs:flex-col ">
